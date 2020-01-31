c0106bd52c3d1b6b99faecd7fc59bdc2117be952
Ma <Fangyao.Ma@emc.com>
2020-01-31 16:42:30 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/peacedog123/gomodtest
+
+go 1.12
---
+++ hi.go
@@ -0,0 +1,9 @@
+package gomodtest
+
+import (
+    "fmt"
+)
+
+func Hi(name string) string {
+    return fmt.Sprintf("Hi, %s", name)
+}
